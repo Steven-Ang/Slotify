a648fcdc70e45913e1ba8e1e5ce5b90b22ca3854
Steven Ang <spikegreen2@gmail.com>
2018-09-20 17:30:17 +0800

    Move the functions to separate folders

--- register.php
+++ register.php
@@ -1,40 +1,9 @@
-<?php
+<?php 
 
-function sanitizeFormPassword($inputText) {
-  $inputText = strip_tags($inputText);
-  return $inputText;
-}
-
-function sanitizeFormUsername($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  return $inputText;
-}
-
-function sanitizeFormString($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  $inputText = ucfirst(strtolower($inputText));
-  return $inputText;
-}
-
-if (isset($_POST["loginButton"])) {
-  
-}
-
-if (isset($_POST["registerButton"])) {
-  $username = sanitizeFormUsername($_POST["username"]);
-  $firstName = sanitizeFormString($_POST["firstName"]);  
-  $lastName = sanitizeFormString($_POST["lastName"]);  
-  $email = sanitizeFormString($_POST["email"]);  
-  $confirmEmail = sanitizeFormString($_POST["confirmEmail"]);  
-  $password = sanitizeFormPassword($_POST["password"]);
-  $confirmPassword = sanitizeFormPassword($_POST["confirmPassword"]);
-  echo "All Good";
-}
+include("inc/handlers/register-handler.php");
+include("inc/handlers/login-handler.php");
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
