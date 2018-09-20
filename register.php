55fce592ef98ef0d0323690edd0b0a3690948af5
Steven Ang <spikegreen2@gmail.com>
2018-09-20 17:21:13 +0800

    Sanitize Inputs

--- register.php
+++ register.php
@@ -1,11 +1,36 @@
 <?php
 
+function sanitizeFormPassword($inputText) {
+  $inputText = strip_tags($inputText);
+  return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+  return $inputText;
+}
+
 if (isset($_POST["loginButton"])) {
   
 }
 
 if (isset($_POST["registerButton"])) {
-  
+  $username = sanitizeFormUsername($_POST["username"]);
+  $firstName = sanitizeFormString($_POST["firstName"]);  
+  $lastName = sanitizeFormString($_POST["lastName"]);  
+  $email = sanitizeFormString($_POST["email"]);  
+  $confirmEmail = sanitizeFormString($_POST["confirmEmail"]);  
+  $password = sanitizeFormPassword($_POST["password"]);
+  $confirmPassword = sanitizeFormPassword($_POST["confirmPassword"]);
+  echo "All Good";
 }
 
 ?>
