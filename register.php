872f3aa5fa3bbab67bcf16dc847c9833b4eb8051
Steven Ang <spikegreen2@gmail.com>
2018-09-20 17:00:18 +0800

    Add register form

--- register.php
+++ register.php
@@ -11,16 +11,61 @@
   <div id="inputContainer">
     <form action="register.php" method="POST" id="loginForm">
       <h2>Login To Your Account</h2>
+
       <div>
         <label for="loginUsername">Username</label>
         <input type="text" id="loginUsername" name="loginUsername" placeholder="Your Username" required>
       </div>
+
       <div>
         <label for="loginPassword">Password</label>
-        <input type="password" id="loginPassword" name="loginPassword" required>
+        <input type="password" id="loginPassword" name="loginPassword" placeholder="Your Password" required>
       </div>
+
       <button type="submit" name="loginButton">Login</button>
     </form>
+
+    <form action="register.php" method="POST" id="registerForm">
+      <h2>Create Your Free Account</h2>
+
+      <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" placeholder="Your Username" required>
+      </div>
+
+      <div>
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
+      </div>
+
+      <div>
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+      </div>
+
+      <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Your Email" required>
+      </div>
+
+      <div>
+        <label for="confirmEmail">Confirm Email</label>
+        <input type="email" id="confirmEmail" name="confirmEmail" placeholder="Confirm Your Email" required>
+      </div>
+
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="loginPassword" placeholder="Your Password" required>
+      </div>
+
+      <div>
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Your Password" required>
+      </div>
+
+      <button type="submit" name="registerButton">Sign Up</button>
+    </form>
+
   </div>
 
 </body>
