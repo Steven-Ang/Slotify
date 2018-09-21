5bb4a7091bada76358c1498fdb2cc2080a0f980c
Steven Ang <spikegreen2@gmail.com>
2018-09-21 21:30:13 +0800

    Add nowPlayingBar

--- index.php
+++ index.php
@@ -18,11 +18,12 @@ if (isset($_SESSION['userLoggedIn'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Welcome to Slotify!</title>
 </head>
 <body>
   <div id="nowPlayingBar">
-    hello
+    
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
