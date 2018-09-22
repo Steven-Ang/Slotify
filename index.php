3f63a3c81bc91c9b01d3100b15a99bccb6c7e6e2
Steven Ang <spikegreen2@gmail.com>
2018-09-22 16:15:45 +0800

    Add nav bar

--- index.php
+++ index.php
@@ -22,88 +22,102 @@ if (isset($_SESSION['userLoggedIn'])) {
   <title>Welcome to Slotify!</title>
 </head>
 <body>
-  <div id="nowPlayingBarContainer">
-    <div id="nowPlayingBar">
+  
+  <div id="mainContainer">
 
-      <div id="nowPlayingLeft">
-        <div class="content">
-          <span class="albumLink">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png" class="albumArtwork" alt="">
-          </span>
-          <div class="trackInfo">
-            <span class="trackName">
-              <span>Brain Damage</span>
-            </span>
+    <div id="topContainer">
 
-            <span class="artistName">
-              <span>Pink Floyd</span>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div id="nowPlayingCenter">
-
-        <div class="content playerControls">
-
-          <div class="buttons">
-            <button class="controlButton shuffle" title="Shuffle Button">
-              <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-            </button>
-
-            <button class="controlButton previous" title="Previous Button">
-              <img src="assets/images/icons/previous.png" alt="Previous">
-            </button>
-
-            <button class="controlButton play" title="Play Button">
-              <img src="assets/images/icons/play.png" alt="Play">
-            </button>
-
-            <button class="controlButton pause" title="Pause Button">
-              <img src="assets/images/icons/pause.png" alt="Pause">
-            </button>
-
-            <button class="controlButton next" title="Next Button">
-              <img src="assets/images/icons/next.png" alt="Next">
-            </button>
-
-            <button class="controlButton repeat" title="Repeat Button">
-              <img src="assets/images/icons/repeat.png" alt="Repeat">
-            </button>
-
-          </div>
-
-        </div>
-
-        <div class="playbackBar">
-          <span class="progressTime current">0.00</span>
-          <div class="progressBar">
-            <div class="progressBarBackground">
-              <div class="progress"></div>
-            </div>
-          </div>
-          <span class="progressTime remaining">0.00</span>
-        </div>
-
-      </div>
-
-      <div id="nowPlayingRight">
-        <div class="volumeBar">
-          <button class="controlButton volume" title="Volume Button">
-            <img src="assets/images/icons/volume.png" alt="Volume">
-          </button>
-
-          <div class="progressBar">
-            <div class="progressBarBackground">
-              <div class="progress"></div>
-            </div>
-          </div>
-
-        </div>
+      <div id="navBarContainer">
+        
       </div>
 
     </div>
+
+    <div id="nowPlayingBarContainer">
+      <div id="nowPlayingBar">
+
+        <div id="nowPlayingLeft">
+          <div class="content">
+            <span class="albumLink">
+              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png" class="albumArtwork" alt="">
+            </span>
+            <div class="trackInfo">
+              <span class="trackName">
+                <span>Brain Damage</span>
+              </span>
+
+              <span class="artistName">
+                <span>Pink Floyd</span>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div id="nowPlayingCenter">
+
+          <div class="content playerControls">
+
+            <div class="buttons">
+              <button class="controlButton shuffle" title="Shuffle Button">
+                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+              </button>
+
+              <button class="controlButton previous" title="Previous Button">
+                <img src="assets/images/icons/previous.png" alt="Previous">
+              </button>
+
+              <button class="controlButton play" title="Play Button">
+                <img src="assets/images/icons/play.png" alt="Play">
+              </button>
+
+              <button class="controlButton pause" title="Pause Button">
+                <img src="assets/images/icons/pause.png" alt="Pause">
+              </button>
+
+              <button class="controlButton next" title="Next Button">
+                <img src="assets/images/icons/next.png" alt="Next">
+              </button>
+
+              <button class="controlButton repeat" title="Repeat Button">
+                <img src="assets/images/icons/repeat.png" alt="Repeat">
+              </button>
+
+            </div>
+
+          </div>
+
+          <div class="playbackBar">
+            <span class="progressTime current">0.00</span>
+            <div class="progressBar">
+              <div class="progressBarBackground">
+                <div class="progress"></div>
+              </div>
+            </div>
+            <span class="progressTime remaining">0.00</span>
+          </div>
+
+        </div>
+
+        <div id="nowPlayingRight">
+          <div class="volumeBar">
+            <button class="controlButton volume" title="Volume Button">
+              <img src="assets/images/icons/volume.png" alt="Volume">
+            </button>
+
+            <div class="progressBar">
+              <div class="progressBarBackground">
+                <div class="progress"></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
