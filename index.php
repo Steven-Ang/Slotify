c05945b8540147f35c4b31cedb82bbbe1e737995
Steven Ang <spikegreen2@gmail.com>
2018-09-24 21:35:34 +0800

    Separation of concerns

--- index.php
+++ index.php
@@ -27,115 +27,11 @@ if (isset($_SESSION['userLoggedIn'])) {
 
     <div id="topContainer">
 
-      <div id="navBarContainer">
-        <nav class="navBar">
-          <a href="index.php" class="logo">
-            <img src="assets/images/spotify.png" alt="">
-          </a>
-          <div class="group">
-            <div class="navItem">
-              <a href="search.php" class="navItemLink">Search</a>
-              <img src="assets/images/icons/search.png" class="icon" alt="Search">
-            </div>
-          </div>
-          <div class="group">
-            <div class="navItem">
-              <a href="browse.php" class="navItemLink">Browse</a>
-            </div>
-            <div class="navItem">
-              <a href="yourMusic.php" class="navItemLink">Your Music</a>
-            </div>
-            <div class="navItem">
-              <a href="profile.php" class="navItemLink">Steven Ang</a>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <?php include("inc/navBar.php"); ?>
 
     </div>
 
-    <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
-
-        <div id="nowPlayingLeft">
-          <div class="content">
-            <span class="albumLink">
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png" class="albumArtwork" alt="">
-            </span>
-            <div class="trackInfo">
-              <span class="trackName">
-                <span>Brain Damage</span>
-              </span>
-
-              <span class="artistName">
-                <span>Pink Floyd</span>
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div id="nowPlayingCenter">
-
-          <div class="content playerControls">
-
-            <div class="buttons">
-              <button class="controlButton shuffle" title="Shuffle Button">
-                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-              </button>
-
-              <button class="controlButton previous" title="Previous Button">
-                <img src="assets/images/icons/previous.png" alt="Previous">
-              </button>
-
-              <button class="controlButton play" title="Play Button">
-                <img src="assets/images/icons/play.png" alt="Play">
-              </button>
-
-              <button class="controlButton pause" title="Pause Button">
-                <img src="assets/images/icons/pause.png" alt="Pause">
-              </button>
-
-              <button class="controlButton next" title="Next Button">
-                <img src="assets/images/icons/next.png" alt="Next">
-              </button>
-
-              <button class="controlButton repeat" title="Repeat Button">
-                <img src="assets/images/icons/repeat.png" alt="Repeat">
-              </button>
-
-            </div>
-
-          </div>
-
-          <div class="playbackBar">
-            <span class="progressTime current">0.00</span>
-            <div class="progressBar">
-              <div class="progressBarBackground">
-                <div class="progress"></div>
-              </div>
-            </div>
-            <span class="progressTime remaining">0.00</span>
-          </div>
-
-        </div>
-
-        <div id="nowPlayingRight">
-          <div class="volumeBar">
-            <button class="controlButton volume" title="Volume Button">
-              <img src="assets/images/icons/volume.png" alt="Volume">
-            </button>
-
-            <div class="progressBar">
-              <div class="progressBarBackground">
-                <div class="progress"></div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
-    </div>
+    <?php include("inc/nowPlayingBar.php"); ?>
 
   </div>
 
