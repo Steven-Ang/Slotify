2e7729df8a72cdb8044ab9cdf9fc17f20d1e7852
Steven Ang <spikegreen2@gmail.com>
2018-09-24 21:59:21 +0800

    Add header and footer php

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+      </div>
+
+    </div>
+
+    <?php include("inc/nowPlayingBar.php"); ?>
+
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<?php
+
+include("inc/config.php");
+
+// session_destroy();
+
+if (isset($_SESSION['userLoggedIn'])) {
+  $userLoggedIn = $_SESSION["userLoggedIn"];
+} else {
+  header("Location: register.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Welcome to Slotify!</title>
+</head>
+<body>
+  
+  <div id="mainContainer">
+
+    <div id="topContainer">
+
+      <?php include("inc/navBar.php"); ?>
+
+      <div id="mainViewContainer">
+
+        <div id="mainContent">
