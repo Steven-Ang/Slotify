89c02159f01879eb1ec8b76051d57be362689203
Steven Ang <spikegreen2@gmail.com>
2018-09-24 22:57:47 +0800

    Fetch the albums from MySQL

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php include("inc/header.php"); ?>
+
+<h2 class="pageHeading">You Might Also Like</h2>
+
+<div class="row">
+
+  <?php 
+
+  $query = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+  while($row = mysqli_fetch_array($query)) {
+    $albumTitle = $row["title"];
+    if (strlen($albumTitle) > 30) {
+      $albumTitle = strstr(wordwrap($albumTitle, 30), "\n", true)."...";
+    }
+    $output = "<div class='col s3 album'>";
+    $output .= "<a href='album.php?id=".$row["id"] . "'>";
+    $output .= "<img class='artwork' src='";
+    $output .= $row["artwork"];
+    $output .= "'>";
+    $output .= "<p class='albumTitle'>".$albumTitle."</p>";
+    $output .= "</div>";
+    $output .= "</a>";
+    echo $output;
+  }
+
+  ?>
+
+</div>
           
 <?php include("inc/footer.php"); ?>
