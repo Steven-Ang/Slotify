13c544c90a0b48fcfa61bce1c8b9ca920f40972e
Steven Ang <spikegreen2@gmail.com>
2018-09-25 23:50:32 +0800

    Fetch artist info from MySQL

--- album.php
+++ album.php
@@ -10,7 +10,14 @@ if (isset($_GET["id"])) {
 
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumID'");
 $album = mysqli_fetch_array($albumQuery);
+
+$artistId = $album["artist"];
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
 echo $album["title"];
+echo "<br>";
+echo $artist["name"];
 
 ?>
 
