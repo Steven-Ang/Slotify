573b1c88bfa150374626dae4a01d4716ab6dfcf6
Steven Ang <spikegreen2@gmail.com>
2018-09-29 00:36:57 +0800

    Add duration

--- assets/js/Audio.js
+++ assets/js/Audio.js
@@ -2,6 +2,18 @@ class Audio {
   constructor() {
     this.audio = document.createElement("audio"); 
     this.currentlyPlaying;
+    this.audio.addEventListener("canplay", () => {
+      $(".progressTime.remaining").text(this.formatTime(this.audio.duration));
+    });
+  }
+
+  formatTime(duration) {
+    let time = Math.round(duration);
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+    let extraZero = (seconds < 10) ? "0" : "";
+    
+    return `${minutes}:${extraZero}${seconds}`;
   }
 
   setTrack(track) {
