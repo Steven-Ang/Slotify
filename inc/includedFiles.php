9acf55af66eabff6f2b79b5ac2e5d0b51988fff9
Steven Ang <spikegreen2@gmail.com>
2018-10-13 18:28:35 +0800

    Add User class

--- inc/includedFiles.php
+++ inc/includedFiles.php
@@ -2,9 +2,17 @@
 
 if (isset($_SERVER["HTTP_X_REQUESTED_WITH"])) {
   include("inc/config.php");
+  include("inc/classes/User.php");
   include("inc/classes/Artist.php");
   include("inc/classes/Album.php");
   include("inc/classes/Song.php");
+  
+  if (isset($_GET["userLoggedIn"])) {
+    $userLoggedIn = new User($con, $_GET["userLoggedIn"]);
+  } else {
+    echo "Username variable was not passed into the page.";
+    exit();
+  }
 } else {
   include("inc/header.php");
   include("inc/footer.php");
