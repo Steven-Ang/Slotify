3f2c777c17b0fdf83ee44598d33bdfb090ab4523
Steven Ang <spikegreen2@gmail.com>
2018-10-13 18:45:27 +0800

    Output the playlists

--- yourMusic.php
+++ yourMusic.php
@@ -7,4 +7,31 @@ include("inc/includedFiles.php");
   <div class="buttons">
     <button class="button green text-center" style="display: block; margin: 15px auto;" onclick="createPlaylist()">New Playlist</button>
   </div>
+
+  <div class="row padding-left">
+  <?php
+
+  $username = $userLoggedIn->getUsername();
+
+  $query = mysqli_query($con, "SELECT * FROM playlist WHERE owner='$username'");
+
+  if (mysqli_num_rows($query) === 0) {
+    echo "<span class='noResults'>"."You don't have any playlist.</span>";
+  }
+
+  while($row = mysqli_fetch_array($query)) {
+
+    $output = "<div class='col s12 m3 album' style='padding: 0 20px 0 0;'>";
+    $output .= "<div class='playlistImage'>";
+    $output .= "<img src='assets/images/icons/playlist.png'>";
+    $output .= "</div>";
+    $output .= $row["name"];
+    $output .= "</div>";
+    echo $output;
+  }
+
+  ?>
+
+</div>
+
 </div>
